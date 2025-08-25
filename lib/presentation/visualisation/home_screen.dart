2af729c92382ce422e8dc812e517631b6619650c
Bonheur Tam <bonheurnouba@gmail.com>
2025-08-25 18:28:02 +0000

    Stable Point

--- lib/presentation/visualisation/home_screen.dart
+++ lib/presentation/visualisation/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:ecofier_viz/core/constants.dart';
+import 'package:ecofier_viz/presentation/authentication/state/login_cubit/login_cubit.dart';
 import 'package:ecofier_viz/presentation/visualisation/widgets/app_options_selector.dart';
 import 'package:ecofier_viz/presentation/visualisation/widgets/weighing_summary_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,72 +13,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const _filtersOptionList = [
+    {
+      "id": "1",
+      "code": "D'aujourd'hui",
+    },
+    {
+      "id": "2",
+      "code": "De la semaine",
+    },
+    {
+      "id": "3",
+      "code": "Du mois",
+    },
+    {
+      "id": "4",
+      "code": "Autre",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    var optionsList = [
-      {
-        "id": "1",
-        "code": "D'aujourd'hui",
-      },
-      {
-        "id": "2",
-        "code": "De la semaine",
-      },
-      {
-        "id": "3",
-        "code": "Du mois",
-      },
-      {
-        "id": "4",
-        "code": "Autre",
-      },
-    ];
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             // Bannière image
-            SizedBox(
-              height: 216,
-              child: Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(64),
-                    ),
-                    child: Image.asset(
-                      'lib/core/assets/bg_1.jpeg',
-                      fit: BoxFit.cover,
-                      height: 192,
-                      width: double.infinity,
-                      errorBuilder: (context, error, stackTrace) => Container(
-                        color: Colors.grey[300],
-                        child: const Center(child: Icon(Icons.image, size: 80)),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 16,
-                    child: Container(
-                      width: 72,
-                      height: 72,
-                      decoration: BoxDecoration(
-                        color: AppColors.white,
-                        border: Border.all(color: AppColors.green1),
-                        borderRadius: BorderRadius.circular(38),
-                      ),
-                      child: const Icon(
-                        Icons.person,
-                        color: AppColors.green1,
-                        size: 32,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            _buildBanner(),
             const Padding(
               padding: EdgeInsets.all(16),
               child: Text(
@@ -89,91 +53,199 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             AppOptionSelector(
-              dataList: optionsList,
+              dataList: _filtersOptionList,
               value: (value) {},
             ),
             const SizedBox(height: 16),
-            Container(
-              margin: const EdgeInsets.all(16),
-              padding: const EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color.fromARGB(255, 230, 237, 231)
-                        .withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      "Pesées",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: AppColors.green2,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  GridView.builder(
-                    padding: const EdgeInsets.all(0),
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3 / 2,
-                    ),
-                    itemCount: 4,
-                    itemBuilder: (context, index) {
-                      return const WeighingSummaryItem();
-                    },
-                  ),
-                ],
-              ),
-            ),
+            _buildWeighingSummary(),
 
             // Create history
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      "Historique",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: AppColors.green2,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  ListView.builder(
-                    padding: EdgeInsets.all(0),
-                    itemCount: 5,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text("Item $index"),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
+            _buildWeighingList(),
           ],
         ),
+      ),
+    );
+  }
+
+  Padding _buildWeighingList() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Historique",
+              style: TextStyle(
+                fontSize: 18,
+                color: AppColors.green2,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListView.builder(
+            padding: EdgeInsets.all(0),
+            itemCount: 5,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text("Item $index"),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildWeighingSummary() {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 230, 237, 231).withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Pesées",
+              style: TextStyle(
+                fontSize: 18,
+                color: AppColors.green2,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          GridView.builder(
+            padding: const EdgeInsets.all(0),
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+            ),
+            itemCount: 4,
+            itemBuilder: (context, index) {
+              return const WeighingSummaryItem();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  SizedBox _buildBanner() {
+    return SizedBox(
+      height: 216,
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(64),
+            ),
+            child: Image.asset(
+              'lib/core/assets/bg_1.jpeg',
+              fit: BoxFit.cover,
+              height: 192,
+              width: double.infinity,
+              errorBuilder: (context, error, stackTrace) => Container(
+                color: Colors.grey[300],
+                child: const Center(child: Icon(Icons.image, size: 80)),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 16,
+            child: Container(
+              width: 72,
+              height: 72,
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                border: Border.all(color: AppColors.green1),
+                borderRadius: BorderRadius.circular(38),
+              ),
+              child: const Icon(
+                Icons.person,
+                color: AppColors.green1,
+                size: 32,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 24,
+            left: 0,
+            right: 0,
+            child: SafeArea(
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Akwaba,",
+                            style: TextStyle(
+                              color: AppColors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          BlocBuilder<LoginCubit, LoginState>(
+                            builder: (context, state) {
+                              if (state is LoginSuccess) {
+                                return Text(
+                                  state.user.username,
+                                  style: const TextStyle(
+                                    color: AppColors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                );
+                              } else {
+                                return const SizedBox.shrink();
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          color: AppColors.white,
+                          onPressed: () {},
+                          icon: const Icon(Icons.notifications),
+                        ),
+                        IconButton(
+                          color: AppColors.white,
+                          onPressed: () {},
+                          icon: const Icon(Icons.menu),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
